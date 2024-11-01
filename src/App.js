daf5b679894665df2be6d6a2fbad98126f9123a3
Your Name <you@example.com>
2024-11-01 17:36:00 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Register from './components/Register';
 import Login from './components/Login';
 
 function App() {
   return (
+   
+
     <Router>
-      <Switch>
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
-      </Switch>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </Router>
   );
 
